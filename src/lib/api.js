8ba7595643c9bf79d2b4a07c203bcaae3b159f4e
scoz <coz.steven@gmail.com>
2025-05-16 09:54:03 +0200

    Remove simulated delay in fetchQuestions function

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,9 +3,6 @@ export async function fetchQuestions() {
       const res = await fetch('https://quizzapi.jomoreschi.fr/api/v1/quiz?limit=5');
       if (!res.ok) throw new Error('Échec du chargement');
       const data = await res.json();
-
-       // Simule un délai de 2 secondes
-      await new Promise(resolve => setTimeout(resolve, 2000));
   
       // Transformation des données vers ton format : { question, choices[], answer }
       return data.quizzes.map((item, index) => ({
