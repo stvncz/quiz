acd042a33a7de5fc7dd22d53579a776594e58484
scoz <coz.steven@gmail.com>
2025-05-16 15:09:18 +0200

    Update API fetch logic to use environment variable and adjust data mapping in fetchQuestions function

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,15 +1,17 @@
+const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:3000';
+
 export async function fetchQuestions() {
     try {
-      const res = await fetch('https://quizzapi.jomoreschi.fr/api/v1/quiz?limit=5');
+      const res = await fetch(`${API_BASE}/api/quiz`);
       if (!res.ok) throw new Error('Échec du chargement');
       const data = await res.json();
   
       // Transformation des données vers ton format : { question, choices[], answer }
-      return data.quizzes.map((item, index) => ({
+      return data.quiz.map(item => ({
         question: item.question,
-        choices: [...item.badAnswers, item.answer],
-        answer: item.answer
-      }));
+        choices: item.choices,
+        id: item.id
+       }));
     } catch (error) {
       console.error('Erreur dans fetchQuestions:', error);
       return [];
